9fa2f54e8123aa9246efb4f54cd43a3fbca377e0
savitri dhameliya <shavitridhameliya@gmail.com>
2025-03-26 17:27:45 +0530

    support 2 section in support page

--- pages/support.jsx
+++ pages/support.jsx
@@ -2,6 +2,7 @@ import { React } from "react";
 import "../public/css/support.css";
 import "../public/css/styles.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const support = () => {
   return (
@@ -10,10 +11,8 @@ const support = () => {
         className="first-section"
         style={{ paddingTop: "120px", paddingBottom: "40px" }}
       >
-        <div className="black-overlays">
-
-        </div>
-        <div className="container " style={{paddingTop:'20px'}}>
+        <div className="black-overlays"></div>
+        <div className="container " style={{ paddingTop: "20px" }}>
           <div className="row gap-3 justify-content-lg-start ">
             {/* <div className="dots-vector-contain">
               <div className="dots-content" style={{top:"-20%"}}>
@@ -31,7 +30,6 @@ const support = () => {
             <div className="col-lg-5 d-flex flex-column justify-content-end">
               <div className="support1-wrapper">
                 <h2>{`ADDL Support Team Is Always There For You, Whenever You Need Us.`}</h2>
-                
               </div>
             </div>
             <div className="col-5">
@@ -61,14 +59,19 @@ const support = () => {
                     height={500}
                     alt="support-img5"
                   ></Image>
-                  <div className=" mb-0 fw-bold text-center mt-2 " style={{color: "#005e8a"}}>ACE Digital Dental Lab.</div>
+                  <div
+                    className=" mb-0 fw-bold text-center mt-2 "
+                    style={{ color: "#005e8a" }}
+                  >
+                    ACE Digital Dental Lab.
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section className="support-content">
+      {/* <section className="support-content">
         <div className="container">
          <div className="row justify-content-center mb-5">
           <div className="col-lg-5 col-xl-4 col-md-5 col-sm-12 col-12">
@@ -86,10 +89,67 @@ const support = () => {
           </div>
          </div>
         </div>
+      </section> */}
+      <section className="third-section" style={{ paddingTop: "150px" }}>
+        <div className="container position-relative pb-5">
+          <div className="dots-vector-contain">
+            <div
+              className="dots-content"
+              style={{ top: "0%", zIndex: "1", width: "50%" }}
+            >
+              <div
+                className="dots-vector"
+                style={{
+                  width: "100%",
+                  height: "75%",
+                  backgroundImage:
+                    "radial-gradient(#fff 1.3px, #0000 1.5px), radial-gradient(#fff 1px, #23545400 1px)",
+                  zIndex: "2",
+                  position: "relative",
+                }}
+              ></div>
+              <div
+                className="dots-wid"
+                style={{ background: "none" }}
+                // style={{transform:"rotate(153deg)"}}
+                //  style={{background: "linear-gradient(150deg, #fff0 1.70596%, #fff 55.2707%)"}}
+              ></div>
+            </div>
+          </div>
+          {/* <img src="/images/line-vector2.png" className="img-fluid line-vector"></img> */}
+          <img
+            src="/images/vector-support6.png"
+            className=" img-fluid vector-support6"
+          ></img>
+          <div className="row mb-5">
+            <h2 className="support-text">In a hurry? We've got you covered.</h2>
+            <div className="col-6">
+              <img
+                src="/images/support-team-img1.png"
+                className="support-team-img6 img-fluid"
+              ></img>
+            </div>
+            <div className="col-4">
+              <p>
+                Our dedicated support team is committed to ensuring smooth and
+                efficient service, prioritizing time-saving solutions and quick
+                delivery. With a focus on responsiveness and reliability, they
+                work closely with you to meet deadlines and streamline
+                processes, helping to deliver high-quality dental restorations
+                in the shortest possible time. Their expertise ensures that
+                every step is handled with care, so you can rely on fast and
+                efficient results without compromising on quality.
+              </p>
+              <Link href="/contactUs">
+                <button className="btn btn-getInTouch">Get In Touch</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="experience-section position-relative">
         <img
-          src="/images/support-vector.png"
+          src="/images/vector-img.png"
           className="img-fluid experience-vector"
         ></img>
         <div className="container">
@@ -116,22 +176,24 @@ const support = () => {
                     step of the way.
                   </p>
                 </div>
-              
               </div>
               <p className="d-block d-sm-block d-md-none">
-                    Our support team brings a wealth of experience and expertise
-                    to every interaction, ensuring that you receive the best
-                    possible service. With years of industry knowledge, they are
-                    equipped to handle any questions or challenges that may
-                    arise, providing expert guidance and personalized solutions.
-                    Their in-depth understanding of the process allows them to
-                    offer valuable insights, ensuring smooth and efficient
-                    outcomes. You can count on our team to deliver reliable,
-                    professional support that enhances your experience every
-                    step of the way.
-                  </p>
+                Our support team brings a wealth of experience and expertise to
+                every interaction, ensuring that you receive the best possible
+                service. With years of industry knowledge, they are equipped to
+                handle any questions or challenges that may arise, providing
+                expert guidance and personalized solutions. Their in-depth
+                understanding of the process allows them to offer valuable
+                insights, ensuring smooth and efficient outcomes. You can count
+                on our team to deliver reliable, professional support that
+                enhances your experience every step of the way.
+              </p>
             </div>
             <div className="col-lg-4 col-md-5 col-sm-12 col-12 order-1 order-md-0 position-relative">
+              <img
+                src="/images/vector-img.png"
+                className="img-fluid experience-vector1"
+              ></img>
               <img src="/images/experience-img.png" className="img-fluid"></img>
               <div className="addl-content">
                 <ul>
@@ -144,6 +206,54 @@ const support = () => {
                 </span>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="go-off-track-section">
+        <div className="container">
+          {/* <img src="/images/vector-support.png" className="img-fluid"></img> */}
+          <img
+            src="/images/vector-support3.png"
+            className="d-none d-md-block support-vector1 img-fluid"
+          ></img>
+          <img
+            src="/images/vector-support4.png"
+            className="d-block d-md-none support-vector1 img-fluid"
+          ></img>
+          <div className="row justify-content-center">
+            <div className="col-lg-5 col-md-6 col-10 order-2 order-md-0">
+              {/* <h2 className="d-block d-md-none">Did Something Go Off Track?</h2> */}
+              <img
+                src="/images/support-4.png"
+                className="d-none d-md-block go-off-track-img img-fluid"
+              ></img>
+            </div>
+            <div className="col-lg-6 col-md-6 col-10 order-1 order-md-0">
+              <h2>Did Something Go Off Track?</h2>
+              <p>
+                Our support team is always ready to assist with any changes or
+                customizations to your current order. Whether you need
+                adjustments to the design, materials, or specifications, they
+                work closely with you to ensure your needs are met. With their
+                expertise and attention to detail, they make the process
+                seamless, ensuring that your order is tailored to your exact
+                requirements. You can rely on our team to provide quick,
+                efficient solutions that meet your expectations, all while
+                maintaining the highest quality standards.
+              </p>
+              <img
+                src="/images/support-4.png"
+                className="d-block d-md-none go-off-track-img img-fluid"
+              ></img>
+              <div className="support-img-wrapper">
+                {/* <img src="/images/support3.png" className="img-fluid"></img> */}
+                <img
+                  src="/images/support-5.png"
+                  className="support-img img-fluid"
+                ></img>
+              </div>
+            </div>
+            {/* <img src="/images/vector-support.png" className="w-25 support-vector1 img-fluid"></img> */}
           </div>
         </div>
       </section>
